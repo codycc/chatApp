40c83e58c9af5bdd2a9d39ae17613fc5dbb8a007
Cody Condon <codycondon50@gmail.com>
2016-11-03 21:46:59 -0400

    Initial commit, view controllers set up

---
+++ chatApp/ChatVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChatVC.swift
+//  chatApp
+//
+//  Created by Cody Condon on 2016-11-03.
+//  Copyright © 2016 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
