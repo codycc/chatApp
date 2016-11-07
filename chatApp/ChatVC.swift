39097d192bb823671593b7b62f5b2bd25855034a
Cody Condon <codycondon50@gmail.com>
2016-11-07 17:12:14 -0500

    add JSQMessagesViewController

--- chatApp/ChatVC.swift
+++ chatApp/ChatVC.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ChatVC: UIViewController {
 
+
+
+class ChatVC: JSQMessagesViewController {
+
+    var messages = [JSQMessage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.senderId = "1"
+        self.senderDisplayName = "CODY"
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        print("press send button")
+        print("\(text)")
+        print("\(senderId)")
+        print("\(senderDisplayName)")
+        messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text))
+        print(messages)
+    }
+    
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        print("didpressaccessorybutton")
     }
 
  
